e8f86eded60a3b875a93a2bfe350bcfbd05b00dc
Denis Soloviev <dionisio14solo14@gmail.com>
2018-11-15 11:20:13 +0200

    responsive

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,33 @@
 $(function() {
-  $(".owl-carousel").owlCarousel({
-	  	nav: true,
-	  	margin: 60,
-	  	navText: ["<i class='fa fa-arrow-left'></i>", "<i class='fa fa-arrow-right'></i>"]
-  });
+	$(".owl-carousel").owlCarousel({
+		nav: true,
+		margin: 60,
+		navText: ["<i class='fa fa-arrow-left'></i>", "<i class='fa fa-arrow-right'></i>"],
+		responsiveClass:true,
+		responsive : {
+			768 : {
+				center : true,
+				margin : 40
+			}
+		}
+	});
+	
+	var $hamburger = $('.header .header__left .hamburger-menu'),
+		$mainMenu = $('.main .main__popup-menu');
+	$hamburger.click(function() {
+		$(this).toggleClass('hamburger-open');
+		if($(this).hasClass('hamburger-open')) {
+			$mainMenu.show().animate({
+				opacity: 1
+			}, 600);	
+		} else {
+			$mainMenu.animate({
+				opacity: 0
+			}, 400, function() {
+				$(this).hide();
+			});
+		}
+	});
+	
+	
 });
