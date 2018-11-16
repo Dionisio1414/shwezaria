d84f541dc589a24111edf4df4aa51482fb53757e
Denis Soloviev <dionisio14solo14@gmail.com>
2018-11-16 02:51:10 +0200

    fixes responsive

--- app/js/common.js
+++ app/js/common.js
@@ -3,14 +3,19 @@ $(function() {
 		nav: true,
 		navText: ["<i class='fa fa-arrow-left'></i>", "<i class='fa fa-arrow-right'></i>"],
 		responsive : {
-			992 : {
+			991 : {
 				items: 3,
 				center : true,
 				startPosition: 1
 			},
 			768 : {
 				items: 1
+			},
+			
+			0 : {
+				items: 1
 			}
+			
 		}
 	});
 	
@@ -31,5 +36,19 @@ $(function() {
 		}
 	});
 	
+	
+	$('.main__socials .button-down').click(function() {
+		$('html, body').animate({scrollTop:$('#next-screen').position().top - 60}, 1000);
+	});
+	
+	
+	$(document).bind('mousewheel', function (e) { 
+		var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*10000); 
+		e.preventDefault(); 
+		e.stopPropagation(); 
+		$(document.body).stop().animate({ 
+			 scrollTop : nt 
+		 } , 500);  
+	});
 	
 });
